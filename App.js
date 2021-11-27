e571bce09c6cb2966efd53b90299bb511ce39da3
tu-mo <ngoctuvnu63@gmail.com>
2021-11-27 17:42:34 +0700

    first commit

---
+++ App.js
@@ -0,0 +1,71 @@
+import { StatusBar } from 'expo-status-bar';
+import React, {useEffect, useState}from 'react';
+import { StyleSheet, Text, View, ImageBackground, } from 'react-native';
+import * as Location from 'expo-location';
+
+import DateTime from './components/DateTime';
+import WeatherScroll from './components/WeatherScroll';
+
+const API_KEY ='49cc8c821cd2aff9af04c9f98c36eb74';
+
+export default function App() {
+  const [data, setData] = useState({});
+
+  useEffect(() => {
+    (async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      
+      if (status !== 'granted') {
+        fetchDataFromApi("40.7128", "-74.0060")
+        return;
+      }
+
+      let location = await Location.getCurrentPositionAsync({});
+      fetchDataFromApi(location.coords.latitude, location.coords.longitude);
+    })();
+  }, [])
+
+  const fetchDataFromApi = (latitude, longitude) => {
+    if(latitude && longitude) {
+      fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&exclude=minutely&units=metric&appid=${API_KEY}`).then(res => res.json()).then(data => {
+
+      console.log(data)
+      // document.write(data.timezone)
+      setData(data)
+      })
+    }
+    
+  }
+  return (
+    
+    <View style={styles.container}>
+      <ImageBackground source={require('./assets/cloudy.jpeg')} style={styles.image}>
+        <DateTime current={data.current} timezone={data.timezone} lat={data.lat} lon={data.lon}/>
+        {/* <DailyScroll weatherData={data.hourly} current={data.current}/> */}
+        <WeatherScroll weatherData={data} current={data.current}/>
+        <Text style={styles.menu}>PINEAPPLE</Text>
+        
+      </ImageBackground>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  image: {
+    flex: 1,
+    resizeMode:"cover",
+    justifyContent:"center"
+  },
+  menu: {
+    // flex:,
+    textAlign:'center',
+    flexGrow:0.1,
+    color:'#eee',
+    fontSize: 20,
+    // fontWeight: '100',
+    padding:10
+}
+});
